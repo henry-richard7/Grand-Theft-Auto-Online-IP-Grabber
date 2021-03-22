6b370484ca8844919d617f5f23a6fc743b7953c8
Henry Richard J <68910039+henry-richard7@users.noreply.github.com>
2021-03-22 13:43:22 +0530

    Upload Files

---
+++ GTA Online IP Grabber/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace GTA_Online_IP_Grabber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Column_IP = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ColumnCountry = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Column_City = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnState = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnZipCode = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnTimeZone = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 43);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(838, 31);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(896, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 94);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start Capture";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(867, 100);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select Interface";
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Column_IP,
+            this.ColumnCountry,
+            this.Column_City,
+            this.columnState,
+            this.columnZipCode,
+            this.columnTimeZone});
+            this.listView1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 131);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1033, 548);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // Column_IP
+            // 
+            this.Column_IP.Text = "IP Address";
+            this.Column_IP.Width = 230;
+            // 
+            // ColumnCountry
+            // 
+            this.ColumnCountry.Text = "Country";
+            this.ColumnCountry.Width = 230;
+            // 
+            // Column_City
+            // 
+            this.Column_City.Text = "City";
+            this.Column_City.Width = 150;
+            // 
+            // columnState
+            // 
+            this.columnState.Text = "State";
+            this.columnState.Width = 130;
+            // 
+            // columnZipCode
+            // 
+            this.columnZipCode.Text = "Zip Code";
+            this.columnZipCode.Width = 120;
+            // 
+            // columnTimeZone
+            // 
+            this.columnTimeZone.Text = "Time Zone";
+            this.columnTimeZone.Width = 200;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1057, 691);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Grand Theft Auto Online IP Grabber";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Column_IP;
+        private System.Windows.Forms.ColumnHeader ColumnCountry;
+        private System.Windows.Forms.ColumnHeader Column_City;
+        private System.Windows.Forms.ColumnHeader columnState;
+        private System.Windows.Forms.ColumnHeader columnZipCode;
+        private System.Windows.Forms.ColumnHeader columnTimeZone;
+    }
+}
+
---
+++ GTA Online IP Grabber/Form1.cs
@@ -0,0 +1,175 @@
+﻿using PcapDotNet.Core;
+using PcapDotNet.Packets;
+using System;
+using PcapDotNet.Packets.IpV4;
+using PcapDotNet.Packets.Transport;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace GTA_Online_IP_Grabber
+{
+    public partial class Form1 : Form
+    {
+        public List<String> ipAddresses = new List<string>();
+        public Form1()
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+        }
+
+        public List<String> getGeoInfo(String ip2Geo)
+        {
+
+            IP2Location.IPResult oIPResult = new IP2Location.IPResult();
+            IP2Location.Component oIP2Location = new IP2Location.Component();
+
+            List<String> ipInfos = new List<string>();
+
+            try {
+                oIP2Location.IPDatabasePath = Environment.CurrentDirectory + @"\\Location DB\\IP2LOCATION-LITE-DB11.BIN";
+                oIPResult = oIP2Location.IPQuery(ip2Geo);
+
+                if (oIPResult.Status.ToString().Equals("OK"))
+                {
+                    ipInfos.Add(oIPResult.CountryLong);
+                    ipInfos.Add(oIPResult.City);
+                    ipInfos.Add(oIPResult.Region);
+                    ipInfos.Add(oIPResult.ZipCode);
+                    ipInfos.Add(oIPResult.TimeZone);
+
+                    return ipInfos;
+
+                }
+                else
+                {
+                    List<String> errorLists = new List<string>();
+
+                    errorLists.Add(oIPResult.Status.ToString());
+                    errorLists.Add(oIPResult.Status.ToString());
+                    errorLists.Add(oIPResult.Status.ToString());
+                    errorLists.Add(oIPResult.Status.ToString());
+                    errorLists.Add(oIPResult.Status.ToString());
+
+                    return errorLists;
+                }
+            }
+            catch
+            {
+                List<String> errorListse = new List<string>();
+
+                errorListse.Add("No Data in DB");
+                errorListse.Add("No Data in DB");
+                errorListse.Add("No Data in DB");
+                errorListse.Add("No Data in DB");
+                errorListse.Add("No Data in DB");
+
+                return errorListse;
+            }
+
+
+        }
+
+        private void PacketHandler(Packet packet)
+        {
+            
+            IpV4Datagram ip = packet.Ethernet.IpV4;
+            UdpDatagram udp = ip.Udp;
+
+            if (!ipAddresses.Contains(ip.Destination.ToString()))
+            {
+                ipAddresses.Add(ip.Destination.ToString());
+                String[] rows = { ip.Destination.ToString(), getGeoInfo(ip.Destination.ToString())[0],
+                    getGeoInfo(ip.Destination.ToString())[1], 
+                    getGeoInfo(ip.Destination.ToString())[2],
+                    getGeoInfo(ip.Destination.ToString())[3],
+                    getGeoInfo(ip.Destination.ToString())[4]
+                };
+                
+                ListViewItem item = new ListViewItem(rows);
+                listView1.Items.Add(item);
+            }
+
+            
+            
+        }
+        private void DevicePrint(IPacketDevice device)
+        {
+            
+            foreach (DeviceAddress address in device.Addresses)
+            {
+
+
+                
+                if (device.Description != null)
+                    comboBox1.Items.Add(device.Description);
+            }
+            
+        }
+        
+        public IList<LivePacketDevice> allDevices = LivePacketDevice.AllLocalMachine;
+        private void Form1_Load(object sender, EventArgs e)
+            {
+            
+
+            for (int i = 0; i < allDevices.Count; i++)
+            {
+                DevicePrint(allDevices[i]);
+            }
+
+        }
+        
+            public void getUDPPackets()
+        {
+            
+
+            if (allDevices.Count == 0)
+            {
+                Console.WriteLine("No interfaces found! Make sure WinPcap is installed.");
+                return;
+            }
+
+            
+
+            PacketDevice selectedDevice = allDevices[comboBox1.SelectedIndex];
+
+            using (PacketCommunicator communicator =
+                selectedDevice.Open(65536,
+                                PacketDeviceOpenAttributes.Promiscuous, 
+                                1000))
+            {
+                using (BerkeleyPacketFilter filter = communicator.CreateFilter("udp port 6672"))
+                {
+                    
+                    communicator.SetFilter(filter);
+                }
+
+
+
+                try {
+                    communicator.ReceivePackets(0, PacketHandler);
+                }
+                catch
+                {
+                    
+                }
+                
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            Thread thread = new Thread(getUDPPackets) { IsBackground = true };
+            thread.Start();
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
